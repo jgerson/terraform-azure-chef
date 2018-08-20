c6d7448d17cd2e6753b4b787d1800d7447f9d043
Amanda Jennings <ajennings@hashicorp.com>
2018-08-20 14:46:00 -0400

    key change

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -29,7 +29,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
     disable_password_authentication = true
 
     ssh_keys {
-      path     = "/home/ajennings/.ssh"
+      path     = "/home/azureuser/.ssh/authorized_keys"
       key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMlVfLSw1cgoBXt9pUvsmhevU1TmLZCJfR1UK2LJqD9raSbb2n96+1F0q6xqUTA/D9NPSOtIhPx39DSZvBkEd+3CFI2KxrlgeChHe7GBeDWfV8/B5Wwq8hbtDQmqjf8/NtM0yfu8L/9zR0EOiicPlAJmxBWm5wMXYH1y1KUk7qq5sOnWr/lGjKsxoxcKZZiUTIH9lZ6Jr9R4uHz4TwyGgVUpwmRhwT98Wcn01lRF5vc/J2F3pY7ZOMybY8oo5MLEFeLFRHMcHnl3REbkZ1xchvqiGIB27jXxwJ+7W8g7AfSta1PDDixuiehtkQ+UtdMrm5un4wsrxxjDWXgszA7YDV agjennings@Amandas-MBP-2.fios-router.home"
     }
   }
