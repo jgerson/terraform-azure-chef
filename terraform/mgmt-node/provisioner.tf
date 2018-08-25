d332c547a2df97e546879f541edc51ad70097e84
Amanda Jennings <ajennings@hashicorp.com>
2018-08-25 14:50:25 -0400

    update provisioner

--- terraform/mgmt-node/provisioner.tf
+++ terraform/mgmt-node/provisioner.tf
@@ -1,11 +1,31 @@
-variable "chef_provision" { 
-  type                      = "map"
-  description               = "Configuration details for chef server"
+resource "azurerm" "web" {
+  # ...
 
-  default = {
-    server_url              = "https://api.chef.io/organizations/ajennings"
-    user_name               = "ajennings"
-    user_key_path           = "~/.chef/ajennings-chef.pem"
-    recreate_client         = true
-    }
+  provisioner "chef" {
+    attributes_json = <<-EOF
+      {
+        "key": "value",
+        "app": {
+          "cluster1": {
+            "nodes": [
+              "webserver1",
+              "webserver2"
+            ]
+          }
+        }
+      }
+    EOF
+
+    environment     = "_default"
+    run_list        = ["cookbook::recipe"]
+    node_name       = "webserver1"
+    secret_key      = "${file("../encrypted_data_bag_secret")}"
+    server_url      = "https://api.chef.io/organizations/ajennings"
+    recreate_client = true
+    user_name       = "ajennings"
+    user_key        = "${file("../ajennings-chef.pem")}"
+    version         = "12.4.1"
+    # If you have a self signed cert on your chef server change this to :verify_none
+    ssl_verify_mode = ":verify_none"
+  }
 }
