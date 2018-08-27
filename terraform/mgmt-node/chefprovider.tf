56a3dbdb5611f00cbeca2817e0429e2e47703d27
Amanda Jennings <ajennings@hashicorp.com>
2018-08-27 11:12:27 -0400

    ..

---
+++ terraform/mgmt-node/chefprovider.tf
@@ -0,0 +1,22 @@
+# Configure the Chef provider
+provider "chef" {
+  server_url = "https://api.chef.io/organizations/ajennings/"
+
+  # You can set up a "Client" within the Chef Server management console.
+  client_name  = "terraform"
+  key_material = "${var.key_material}"
+}
+
+# Create a Chef Environment
+resource "chef_environment" "production" {
+  name = "production"
+}
+
+# Create a Chef Role
+resource "chef_role" "app_server" {
+  name = "app_server_ajennings"
+
+  run_list = [
+    "recipe[terraform]",
+  ]
+}
