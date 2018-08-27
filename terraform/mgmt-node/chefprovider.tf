e823b25f03963236d0cc2ff97d5227279a8c672d
Amanda Jennings <ajennings@hashicorp.com>
2018-08-27 11:27:29 -0400

    chef update

--- terraform/mgmt-node/chefprovider.tf
+++ terraform/mgmt-node/chefprovider.tf
@@ -3,7 +3,7 @@ provider "chef" {
   server_url = "https://api.chef.io/organizations/ajennings/"
 
   # You can set up a "Client" within the Chef Server management console.
-  client_name  = "terraform"
+  client_name  = "chef-terraform-validator"
   key_material = "${var.key_material}"
 }
 
