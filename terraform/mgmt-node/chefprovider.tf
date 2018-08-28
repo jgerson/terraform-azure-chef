4f32c62ab45ba2b5259619011e24563b3ede9434
Amanda Jennings <ajennings@hashicorp.com>
2018-08-28 12:02:26 -0400

    ..

--- terraform/mgmt-node/chefprovider.tf
+++ terraform/mgmt-node/chefprovider.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_machine" "myterraformvm" {
+resource "azurerm_virtual_machine" "ajennings_vm" {
   name                  = "${local.virtual_machine_name}"
   location              = "${azurerm_resource_group.main.location}"
   resource_group_name   = "${azurerm_resource_group.main.name}"
