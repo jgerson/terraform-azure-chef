2c9273ce8b167386bdd3859af3d4a3bed61babb8
Amanda Jennings <ajennings@hashicorp.com>
2018-08-28 17:56:52 -0400

    Chef update

--- chef-repo/cookbooks/starter/recipes/default.rb
+++ chef-repo/cookbooks/starter/recipes/default.rb
@@ -6,3 +6,16 @@ log "Welcome to Chef, #{node["starter_name"]}!" do
 end
 
 # For more information, see the documentation: https://docs.chef.io/essentials_cookbook_recipes.html
+
+# Install Nginx
+execute "apt-get update" do
+  command "apt-get update"
+end
+
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
