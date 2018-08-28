55353ae7938d294ac385f37b0e12fb01e474ab4e
Amanda Jennings <ajennings@hashicorp.com>
2018-08-28 12:45:02 -0400

    more chef

--- terraform/mgmt-node/compute.tf
+++ terraform/mgmt-node/compute.tf
@@ -40,6 +40,35 @@ resource "azurerm_virtual_machine" "myterraformvm" {
     storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
   }
 
+# ...
+
+  provisioner "chef" {
+    attributes_json = <<-EOF
+      {
+        "key": "value",
+        "app": {
+          "cluster1": {
+            "nodes": [
+              "webserver1"
+            ]
+          }
+        }
+      }
+    EOF
+
+    environment     = "ajennings-test"
+    run_list        = ["cookbook::recipe"]
+    node_name       = "webserver1"
+    server_url      = "https://api.chef.io/organizations/ajennings"
+    recreate_client = true
+    user_name       = "chef-terraform"
+    user_key        = "${var.key_material}"
+    version         = "12.21.1"
+    # If you have a self signed cert on your chef server change this to :verify_none
+    ssl_verify_mode = ":verify_none"
+  }
+}
+
   tags {
     environment = "Terraform Azure Chef Demo"
   }
