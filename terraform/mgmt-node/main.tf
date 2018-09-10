073a4c08c2f03c60dbfadee2aac0e992df9bf119
Amanda Jennings <ajennings@hashicorp.com>
2018-09-10 14:04:23 -0400

    update count

--- terraform/mgmt-node/main.tf
+++ terraform/mgmt-node/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "myterraformgroup" {
   name     = "${var.resource_group_name}"
   location = "${var.resource_group_location}"
-
+  count = 3
   tags {
     environment = "${var.resource_group_tag}"
   }
