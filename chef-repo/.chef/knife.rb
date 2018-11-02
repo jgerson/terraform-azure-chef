edc53317b1b0db16d49a9560ae2434b41d8c0e93
Jeremy Gerson <jgerson@hashicorp.com>
2018-11-02 10:25:15 -0400

    Update knife.rb

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "ajennings"
-client_key               "#{current_dir}/ajennings.pem"
-chef_server_url          "https://api.chef.io/organizations/ajennings"
+node_name                "jgerson"
+client_key               "#{current_dir}/jgerson.pem"
+chef_server_url          "https://api.chef.io/organizations/jgerson"
 cookbook_path            ["#{current_dir}/../cookbooks"]
